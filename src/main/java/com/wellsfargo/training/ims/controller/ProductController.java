8a09bdc94dda744179ce4a6d676a2eef820baba8
Pendyala Sri Harshita <harshitapendyala@gmail.com>
2023-09-14 16:24:02 +0530

    Updatd Code - 14/09

--- src/main/java/com/wellsfargo/training/ims/controller/ProductController.java
+++ src/main/java/com/wellsfargo/training/ims/controller/ProductController.java
@@ -1,12 +1,18 @@
 package com.wellsfargo.training.ims.controller;
 
+import com.wellsfargo.training.ims.exception.ResourceNotFoundException;
 import com.wellsfargo.training.ims.model.Product;
 import com.wellsfargo.training.ims.service.ProductService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*Spring RestController annotation is used to create RESTful web services using Spring MVC.
 		* Spring RestController takes care of mapping request data to the defined request handler method.
@@ -14,6 +20,7 @@ import java.util.List;
 		*
 		* @RequestMapping - maps HTTP request with a path to a controller
 		* */
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping
 public class ProductController {
@@ -24,17 +31,68 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	@PostMapping("/products")
-	public Product saveProduct(@Validated @RequestBody Product product)
+	public ResponseEntity<Product> saveProduct(@Validated @RequestBody Product product)
 	{
+		try{
+			Product p= productService.saveProduct(product);
+			return	ResponseEntity.status(HttpStatus.CREATED).body(p);
+
+		}
+		catch (Exception e){
+			e.printStackTrace();
+
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
 		/* @RequestBody annotation automatically deserializes the JSON into a Java type */
-		Product p= productService.saveProduct(product);
-		return	p;
+
 	}
 	@GetMapping("/products")
-	public List<Product> getAllProducts()
+	public ResponseEntity<List> getAllProducts()
 	{
-		return productService.findall();
+		try{
+			List<Product> products= productService.findall();
+			return ResponseEntity.ok(products);
+		}
+		catch (Exception e){
+			e.printStackTrace();
+
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
+
 	}
+	/* @PathVariable is a Spring annotation which indicates that a method parameter should be
+    *  bound to a URI template variable.
+      @PathVariable annotation is used to read an URL template variable.
+    */
+	//Open PostMan, make a GET Request - http://localhost:8085/ims/api/products/4
+	@GetMapping("/products/{id}")
+	public ResponseEntity<Product> getProductById(@PathVariable(value = "id") Long pid) throws ResourceNotFoundException{
+		Product p = productService.getSingleProduct(pid).orElseThrow(() -> new ResourceNotFoundException("Product Not Found for this Id: "));
+		return ResponseEntity.ok().body(p);
+	}
+	//Open PostMan, make a PUT Request - http://localhost:8085/ims/api/products/1003
+	//Select body -> raw -> JSON
+	//Update JSON product object with new Values.
+	@PutMapping("/products/{id}")
+	public ResponseEntity<Product> updateProduct(@PathVariable(value = "id") Long pid, @Validated @RequestBody Product product) throws ResourceNotFoundException{
+		Product p = productService.getSingleProduct(pid).orElseThrow(() -> new ResourceNotFoundException("Product Not Found for this Id: "));
+		//Update product with new values
+		p.setBrand(product.getBrand());
+		p.setName(product.getName());
+		p.setMadeIn(product.getMadeIn());
+		p.setPrice(product.getPrice());
+		final Product updatedProduct = productService.saveProduct(p);
+		return ResponseEntity.ok().body(updatedProduct);
+	}
+	@DeleteMapping("/products/{id}")
+	public ResponseEntity<Map<String,Boolean>> deleteProduct(@PathVariable(value = "id") Long pid) throws ResourceNotFoundException{
+		productService.getSingleProduct(pid).orElseThrow(()-> new ResourceNotFoundException("Product Not Found"));
+		productService.deleteProduct(pid);
+		Map<String,Boolean> response=new HashMap<String,Boolean>();
+		response.put("Deleted",Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
+
 	
 
 }
--- src/main/java/com/wellsfargo/training/ims/service/ProductService.java
+++ src/main/java/com/wellsfargo/training/ims/service/ProductService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 /*
  * A service layer is an additional layer in an MVC application that
  * mediates communication between a controller and repository layer.
@@ -27,5 +28,11 @@ public class ProductService {
     }
     public List<Product> findall(){
         return productRepository.findAll();
+    }
+    public Optional<Product> getSingleProduct(long id){
+        return productRepository.findById(id);
+    }
+    public void deleteProduct(long id){
+        productRepository.deleteById(id);
     }
 }
